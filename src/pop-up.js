e07240682397a85c6d5860e4f1d5f43a72bf49c1
Jenna Alden <aldenjenna04@gmail.com>
2024-02-25 20:45:28 -0500

    update

---
+++ src/pop-up.js
@@ -0,0 +1,21 @@
+import { LitElement, html, css } from 'lit';
+
+export class popup extends LitElement {
+
+render () {
+    return html`
+     
+
+    
+    
+    
+    
+`}
+
+
+
+
+
+
+} 
+globalThis.customElements.define(popup.tag, popup);
